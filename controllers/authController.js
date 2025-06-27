e58606fb457da52fd08607a2f5c0d451aabea7b9
arungangula <arungangula93@gmail.com>
2025-06-27 13:29:03 -0700

    registering a new user with password hashed using bcrypt

---
+++ controllers/authController.js
@@ -0,0 +1,28 @@
+const bcrypt = require('bcryptjs');
+const { validationResult } = require('express-validator');
+
+const users = [];
+exports.register = async(req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { username, password } = req.body;
+
+    const userExists = users.find(u => u.username == username);
+
+    if (userExists) {
+        return res.status(400).json({message: 'Username already exists'});
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = {
+        id: users.length + 1,
+        username,
+        password: hashedPassword
+    };
+    users.push(user);
+    res.status(201).json({ message: `User ${username} registered successfully` });
+};
--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 3002;
 
+const authRoutes = require('./routes/authRoutes');
+
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+
 app.listen(PORT, () => {
     console.log(`Auth API running on http://localhost:${PORT}`);
 });
